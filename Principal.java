6ccf34eba91f43f5596f540f8df658eb1d086462
Sara Patzan <sarischajon@gmail.com>
2019-02-21 19:02:53 -0600

    commit inicial

---
+++ Principal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Principal {
+
+public static void main (String[] args) {
+
+Scanner myScanner = new Scanner(System.in);
+Random aleatorio = new Random(System.currentTimeMillis());
+int intAletorio = aleatorio.nextInt(10);
+System.out.println("Numero");
+}
+}
